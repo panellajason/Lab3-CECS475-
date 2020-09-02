0e5518f25342bd07758aee94d023ae7bebc7b1df
Jason Panella <panellajason@gmail.com>
2020-09-02 15:33:54 -0700

    Added main and Stock/StockBroker classes

---
+++ Lab3/Stock.cs
@@ -0,0 +1,72 @@
+﻿// Jason Panella
+// John Bui
+// CECS 475
+// September 9, 2020
+// Lab 3
+
+using System;
+using System.Threading;
+
+namespace Lab3
+{
+    public class Stock
+    {
+        private readonly Thread _thread;
+
+        //public event EventHandler<StockNotification> StockEvent;
+
+        public string StockName { get; set; }
+        public int InitialValue { get; set; }
+        public int MaxChange { get; set; }
+        public int Threshold { get; set; }
+        public int CurrentValue { get; set; }
+        public int NumChanges { get; set; }
+
+        public Stock(string name, int startingValue, int maxChange, int threshold)
+        {
+            StockName = name;
+            InitialValue = startingValue;
+            CurrentValue = InitialValue;
+            MaxChange = maxChange;
+            Threshold = threshold;
+
+            _thread = new Thread(new ThreadStart(Activate));
+        }
+
+
+        public void Activate()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Thread.Sleep(500); //1/2 second
+                ChangeStockValue();
+            }
+        }
+
+
+        public void ChangeStockValue()
+        {
+            System.Random random = new System.Random();
+            CurrentValue += random.Next(1,MaxChange - 1);
+            NumChanges++;
+
+            int valueDifference = CurrentValue - InitialValue;
+            if(valueDifference > Threshold)
+
+            {
+                //Raise the event
+
+            }
+        }
+
+
+        /*
+         * 
+         * Event property StockEvent: 
+         * Class EventData from EventArgs to store the event’s data: stock name, 
+         * current stock value and number of changes in the stock.
+         *
+         */
+
+    }
+}
---
+++ Lab3/StockBroker.cs
@@ -0,0 +1,34 @@
+﻿// Jason Panella
+// John Bui
+// CECS 475
+// September 9, 2020
+// Lab 3
+
+using System;
+using System.Collections.Generic;
+
+namespace Lab3
+{
+    public class StockBroker
+    {
+        public string BrokerName { get; set; }
+        public List<Stock> StockLists = new List<Stock>();
+
+        public StockBroker(string name)
+        {
+            BrokerName = name;
+        }
+
+        public void AddStock(Stock stock)
+        {
+            StockLists.Add(stock);
+
+            //stock.stockEvent += EventHandler(___, ____, ___)
+        }
+
+        /* EventHandler(______________________________)
+        *  Display broker name, stock name, current stock value, and number of changes)
+        *  Note: name.PadRight(10) to align the column.
+        */
+    }
+}
